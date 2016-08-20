ee9841fd7ffd442f78a64f6335ec411b5c64f48b
vtsman <hokiespencer@gmail.com>
2016-08-20 11:04:38 -0400

    Update arm-elf-gcc.rb

--- arm-elf-gcc.rb
+++ arm-elf-gcc.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class ArmElfGcc < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
-  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.8.0/gcc-4.8.0.tar.bz2'
+  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.0/gcc-4.9.0.tar.bz2'
+  mirror 'http://ftp.gnu.org/gnu/gcc/gcc-4.9.0/gcc-4.9.0.tar.bz2'
   sha1 'b4ee6e9bdebc65223f95067d0cc1a634b59dad72'
 
   depends_on 'gmp'
