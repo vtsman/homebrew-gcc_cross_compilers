09638ae9a4df755702eb073a5ae866de24001bcc
vtsman <hokiespencer@gmail.com>
2016-08-20 11:05:14 -0400

    Update arm-elf-binutils.rb

--- arm-elf-binutils.rb
+++ arm-elf-binutils.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class ArmElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
+  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.27.tar.gz'
   sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
 
   depends_on 'apple-gcc42' => :build
