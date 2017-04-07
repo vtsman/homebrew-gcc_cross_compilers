c0ff028053c0197a6356d4709e07af8acefe4adb
vtsman <hokiespencer@gmail.com>
2017-04-07 12:03:11 -0400

    Update x64-elf-binutils.rb

--- x64-elf-binutils.rb
+++ x64-elf-binutils.rb
@@ -3,7 +3,7 @@ require 'formula'
 class X64ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
-  sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
+  #sha1 '470c388c97ac8d216de33fa397d7be9f96c3fe04'
 
   depends_on 'apple-gcc42' => :build
 
