d53d2a4981d0a2657f8d324713c3b2c0498a9bb0
vtsman <hokiespencer@gmail.com>
2017-04-07 12:02:57 -0400

    Update i586-elf-gdb.rb

--- i586-elf-gdb.rb
+++ i586-elf-gdb.rb
@@ -3,7 +3,7 @@ require 'formula'
 class I586ElfGdb < Formula
   homepage 'http://gcc.gnu.org'
   url 'http://ftp.gnu.org/gnu/gdb/gdb-7.6.tar.bz2'
-  sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
+  #sha1 'b64095579a20e011beeaa5b264fe23a9606ee40f'
 
   depends_on 'i586-elf-binutils'
   depends_on 'i586-elf-gcc'
